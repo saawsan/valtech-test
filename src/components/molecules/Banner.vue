8d01713e128d922ddb39bce03248c93bfe838a46
saawsan <s.cheffah@gmail.com>
2022-01-23 20:05:52 +0100

    create banner component

---
+++ src/components/molecules/Banner.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="banner">
+    <h1 class="heading heading-1">
+      <span v-if="baseline" v-text="baseline" class="baseline d-block"></span>
+      <logo v-if="withLogo" class="logo" />
+      <template v-else>{{ title }}</template>
+    </h1>
+    <p v-text="content" class="content" />
+    <img :src="background.xl" alt="" class="bg" />
+  </section>
+</template>
+
+<script>
+import Logo from '@/components/atoms/Logo.vue';
+
+export default {
+  name: 'Banner',
+  components: {
+    Logo,
+  },
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+    baseline: {
+      type: String,
+      required: false,
+    },
+    withLogo: {
+      type: String,
+      required: false,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    background: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .banner {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 2rem;
+    padding: 1.3rem 1rem;
+    border-radius: 7px;
+    color: $font-light-color;
+    text-align: center;
+    background: rgba(0,0,0,.3);
+    aspect-ratio: 16 / 9;
+
+    .content {
+      margin: 1rem auto 0;
+      max-width: 35rem;
+    }
+
+    .logo {
+      margin-top: .7rem;
+      width: 10rem;
+      fill: $font-light-color;
+      @include media-bp-up(md) {
+        margin-top: 1.2rem;
+        width: 20rem;
+      }
+    }
+
+    .bg {
+      position: absolute;
+      top: 0; right: 0;
+      border-radius: 7px;
+      width: 100%; height: 100%;
+      object-fit: cover;
+      z-index: -1;
+    }
+  }
+</style>
--- src/components/templates/Dispatch.vue
+++ src/components/templates/Dispatch.vue
@@ -1,42 +1,59 @@
 <template>
-  <div id="dispatch-template">
-    <br> {{ title }}
-    <br> {{ intro }}
-    <br> {{ localities }}
+  <div id="dispatch-template" class="main-wrapper">
+    <div v-if="pending">Placeholder</div>
+    <template v-else>
+      <banner
+        :baseline="title"
+        :content="intro"
+        :background="background"
+        with-logo="true" />
 
-    <h1 class="heading heading-1"><span class="baseline d-block">Italy</span> Venise</h1><br>
-    <h2 class="heading heading-2"><span class="baseline d-block">Italy</span> Venise</h2><br>
-    <a href="#" class="btn">Explore more</a><br>
-    <button type="button" class="btn">Explore more</button><br>
+      <hr>
+      <h1 class="heading heading-1"><span class="baseline d-block">Italy</span> Venise</h1><br>
+      <h2 class="heading heading-2"><span class="baseline d-block">Italy</span> Venise</h2><br>
+      <a href="#" class="btn">Explore more</a><br>
+      <button type="button" class="btn">Explore more</button><br>
+    </template>
   </div>
 </template>
 
 <script>
 import fetchDispatchData from '@/services/dispatch';
+import Banner from '@/components/molecules/Banner.vue';
 
 export default {
   name: 'Dispatch',
   components: {
+    Banner,
   },
   data() {
     return {
       title: null,
       intro: null,
       localities: [],
+      pending: true,
     };
   },
   created() {
-    fetchDispatchData().then(({ title, intro, grid }) => {
+    fetchDispatchData().then(({
+      title, intro, background, grid,
+    }) => {
       this.title = title;
       this.intro = intro;
+      this.background = background;
       this.localities = grid;
     }).catch((error) => {
       // todo: handle error
       console.log('catch', error);
+    }).finally(() => {
+      this.pending = false;
     });
   },
 };
 </script>
 
 <style lang="scss">
+#dispatch-template {
+  margin-top: 4rem;
+}
 </style>
